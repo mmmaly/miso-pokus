ab0e8b9c79c89856786553a7d647d82cbfd64f2f
mmmaly <mmmaly+git@gmail.com>
2014-07-12 15:05:44 +0200

    Update scraper.pl

--- scraper.pl
+++ scraper.pl
@@ -33,8 +33,8 @@ for my $i (1 .. $#rows) #skip 0th (header)
    if($#tds)
    {
    #print encode_json $tds;
-     print encode_json $tds[0][0];
-     print encode_json $tds[4][0];
+     print  $tds[0][0];
+     print  $tds[4][0];
    }
    #$dt->insert([(Datum => $tds[0]->content,     Nazov => $tds[4]->content)]);
 }
